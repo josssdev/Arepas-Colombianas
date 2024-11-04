ce9edc1d5ac5128dfd1f30a5e2198bfdde497ee5
josssdev <134952089+josssdev@users.noreply.github.com>
2024-11-03 21:40:30 -0300

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,20 @@
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: "351891003007"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('Notificación recibida:', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
